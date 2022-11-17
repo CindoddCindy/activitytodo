99e3242c50f3f6693e8930fa8f1acd07624a63c1
cindoddcindy <cindodcindy@gmail.com>
2022-11-17 18:18:07 +0700

    add class

--- activitytodo/src/main/java/com/activitytodo/activitytodo/repository/ActivityRepository.java
+++ activitytodo/src/main/java/com/activitytodo/activitytodo/repository/ActivityRepository.java
@@ -1,5 +1,18 @@
 package com.activitytodo.activitytodo.repository;
 
-public class ActivityRepository {
-  
+import com.activitytodo.activitytodo.model.Activity;
+import com.activitytodo.activitytodo.response.ActivityRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity,Long> {
+  Activity createActivity(ActivityRequest activityRequest);
+
+  Activity getActivityById(Long id);
+
+  Activity deleteActivityById(Activity activity);
+
 }
--- activitytodo/src/main/java/com/activitytodo/activitytodo/response/ResponseHandle.java
+++
@@ -1,9 +0,0 @@
-package com.activitytodo.activitytodo.response;
-
-import java.util.List;
-
-public class ResponseHandle {
-  String status;
-      String message;
-      List<Object> data;
-}
